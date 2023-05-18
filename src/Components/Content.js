97af125076b16ddb5e84f8187e241986b19b2b9e
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-05-18 11:23:28 +0100

    save

---
+++ src/Components/Content.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+function Content() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <main>
+      <Carousel responsive={responsive}>
+        <div><img src="/image/logo.jpeg" className='logo1' alt=''/></div>
+        <div><img src='/image/logo1.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo2.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo3.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo4.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo5.jpeg' className='logo1' alt=''/></div>
+      </Carousel>
+
+      <Carousel responsive={responsive}>
+        <div><img src="/image/logo6.jpeg" className='logo1' alt=''/></div>
+        <div><img src='/image/logo2.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo3.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo4.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo1.jpeg' className='logo1' alt=''/></div>
+        <div><img src='/image/logo11.jpeg'className='logo1' alt=''/></div>
+      </Carousel>
+
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    const today = new Date();
+  return (
+    <footer>
+      copyright  &copy; {today.getFullYear()}
+    </footer>
+  )
+}
+
+export default Footer
